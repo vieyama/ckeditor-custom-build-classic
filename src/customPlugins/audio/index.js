43ed39e29681ab52ee7617a1b4e4485749989c39
vieyama <yoviefp@gmail.com>
2021-08-06 18:19:16 +0700

    update

--- src/customPlugins/audio/index.js
+++ src/customPlugins/audio/index.js
@@ -1,21 +1,16 @@
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import FileDialogButtonView from "@ckeditor/ckeditor5-upload/src/ui/filedialogbuttonview";
+import Notification from "@ckeditor/ckeditor5-ui/src/notification/notification";
+import axios from "axios";
 import imageIcon from "./volume.svg";
 
-async function audioToBase64(audioFile) {
-  return new Promise((resolve, reject) => {
-    let reader = new FileReader();
-    reader.onerror = reject;
-    reader.onload = (e) => resolve(e.target.result);
-    reader.readAsDataURL(audioFile);
-  });
-}
 export default class InsertAudio extends Plugin {
   init() {
     const editor = this.editor;
 
     editor.ui.componentFactory.add("insertAudio", (locale) => {
       const view = new FileDialogButtonView(locale);
+      const notification = new Notification();
 
       view.set({
         acceptedType: "audio/*",
@@ -31,28 +26,59 @@ export default class InsertAudio extends Plugin {
       const uniqID = (
         Date.now().toString(36) + Math.random().toString(36).substr(2, 5)
       ).toUpperCase();
+      notification.showInfo("Editor is offline.", {
+        namespace: "editor:status",
+      });
+      notifications.on(
+        "show:warning",
+        (evt, data) => {
+          console.log(data);
+          // Log the warning to some error tracker.
 
+          // Stop this event to prevent displaying it as an alert.
+          evt.stop();
+        },
+        { priority: "low" }
+      );
       view.on("done", async (evt, files) => {
-        const file = files[0];
-        const url = URL.createObjectURL(file);
+        evt.stop();
 
-        await audioToBase64(file).then((res) => {
-          const audio = `<figure class="audio">
+        var file = files[0];
+
+        const data = new FormData();
+        data.append("bucketName", "media");
+        data.append("storagePath", "question-file/audio");
+        data.append("file", file);
+        data.append("fileName", `question-audio-${uniqID}`);
+
+        await axios
+          .post(
+            "https://dev-storageclient-edoo.mocogawe.com/uploadobject",
+            data,
+            {
+              headers: {
+                "Content-Type": "multipart/form-data",
+              },
+            }
+          )
+          .then((result) => {
+            console.log(result);
+            const audio = `<figure class="audio">
               <audio
                 controls="controls"
                 alt=""
-                src="${res}"
+                src="${result.data.urlPath}"
               >
                 &nbsp;
               </audio>
             </figure>`;
-          const viewFragment = editor.data.processor.toView(audio);
-          const modelFragment = editor.data.toModel(viewFragment);
-          editor.model.insertContent(
-            modelFragment,
-            editor.model.document.selection
-          );
-        });
+            const viewFragment = editor.data.processor.toView(audio);
+            const modelFragment = editor.data.toModel(viewFragment);
+            editor.model.insertContent(
+              modelFragment,
+              editor.model.document.selection
+            );
+          });
       });
       return view;
     });
