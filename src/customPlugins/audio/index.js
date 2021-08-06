25d96ecaedc42bbbb5777333b212a18541be07be
vieyama <yoviefp@gmail.com>
2021-08-06 16:11:32 +0700

    add audio plugin

--- src/customPlugins/audio/index.js
+++ src/customPlugins/audio/index.js
@@ -1,49 +1,59 @@
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
-import ButtonView from "@ckeditor/ckeditor5-ui/src/button/buttonview";
-
+import FileDialogButtonView from "@ckeditor/ckeditor5-upload/src/ui/filedialogbuttonview";
 import imageIcon from "./volume.svg";
 
+async function audioToBase64(audioFile) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = (e) => resolve(e.target.result);
+    reader.readAsDataURL(audioFile);
+  });
+}
 export default class InsertAudio extends Plugin {
   init() {
     const editor = this.editor;
 
     editor.ui.componentFactory.add("insertAudio", (locale) => {
-      const view = new ButtonView(locale);
+      const view = new FileDialogButtonView(locale);
 
       view.set({
-        label: "Insert audio",
+        acceptedType: "audio/*",
+        allowMultipleFiles: false,
+      });
+
+      view.buttonView.set({
+        label: "Insert Audio",
         icon: imageIcon,
         tooltip: true,
       });
 
-      // Callback executed once the image is clicked.
-      view.on("execute", () => {
-        const audioUrl = prompt("Audio URL");
+      const uniqID = (
+        Date.now().toString(36) + Math.random().toString(36).substr(2, 5)
+      ).toUpperCase();
 
-        editor.model.change((writer) => {
-          let audioElement = writer.createElement("AUDIO", {
-            src: audioUrl,
-          });
-          console.log(audioElement);
-          var x = document.createElement("AUDIO");
+      view.on("done", async (evt, files) => {
+        const file = files[0];
+        const url = URL.createObjectURL(file);
 
-          if (x.canPlayType("audio/mpeg")) {
-            x.setAttribute("src", audioUrl);
-          } else {
-            x.setAttribute("src", audioUrl);
-          }
-
-          x.setAttribute("controls", "controls");
-
-          console.log(x);
-
-          const audioFrame = `<figure class="audio"><audio controls="controls" alt="" src="${audioUrl}">&nbsp;</audio></figure><p>&nbsp;</p>`;
-          // Insert the image in the current selection location.
-          console.log(audioFrame);
-          editor.execute("insertHtmlEmbed", audioFrame);
+        await audioToBase64(file).then((res) => {
+          const audio = `<figure class="audio">
+              <audio
+                controls="controls"
+                alt=""
+                src="${res}"
+              >
+                &nbsp;
+              </audio>
+            </figure>`;
+          const viewFragment = editor.data.processor.toView(audio);
+          const modelFragment = editor.data.toModel(viewFragment);
+          editor.model.insertContent(
+            modelFragment,
+            editor.model.document.selection
+          );
         });
       });
-
       return view;
     });
   }
