5dcc3ee43eeae25121e87e9c0d43112bb5a3d523
vieyama <yoviefp@gmail.com>
2021-08-09 18:23:15 +0700

    update

--- src/customPlugins/audio/index.js
+++ src/customPlugins/audio/index.js
@@ -1,16 +1,16 @@
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 import FileDialogButtonView from "@ckeditor/ckeditor5-upload/src/ui/filedialogbuttonview";
-import Notification from "@ckeditor/ckeditor5-ui/src/notification/notification";
 import axios from "axios";
 import imageIcon from "./volume.svg";
-
+import swal from "sweetalert";
+import _ from "lodash";
+const _UPLOAD_FILE_LIMIT = 20971520;
 export default class InsertAudio extends Plugin {
   init() {
     const editor = this.editor;
 
     editor.ui.componentFactory.add("insertAudio", (locale) => {
       const view = new FileDialogButtonView(locale);
-      const notification = new Notification();
 
       view.set({
         acceptedType: "audio/*",
@@ -26,24 +26,21 @@ export default class InsertAudio extends Plugin {
       const uniqID = (
         Date.now().toString(36) + Math.random().toString(36).substr(2, 5)
       ).toUpperCase();
-      notification.showInfo("Editor is offline.", {
-        namespace: "editor:status",
-      });
-      notifications.on(
-        "show:warning",
-        (evt, data) => {
-          console.log(data);
-          // Log the warning to some error tracker.
 
-          // Stop this event to prevent displaying it as an alert.
-          evt.stop();
-        },
-        { priority: "low" }
-      );
       view.on("done", async (evt, files) => {
-        evt.stop();
-
         var file = files[0];
+        const size = file.size;
+
+        if (size > _UPLOAD_FILE_LIMIT) {
+          //50mb이상 파일일 때 notification 보냄
+          swal("Info", "File tidak boleh melebihi 20 MB!", "error");
+          return;
+        }
+
+        swal("Mengunggah audio ...", {
+          buttons: false,
+          closeOnClickOutside: false,
+        });
 
         const data = new FormData();
         data.append("bucketName", "media");
@@ -62,22 +59,19 @@ export default class InsertAudio extends Plugin {
             }
           )
           .then((result) => {
-            console.log(result);
-            const audio = `<figure class="audio">
-              <audio
-                controls="controls"
-                alt=""
-                src="${result.data.urlPath}"
-              >
-                &nbsp;
-              </audio>
-            </figure>`;
+            const audio = `<figure class="audio"><audio controls="controls" alt="" src="${result.data.urlPath}">&nbsp;</audio></figure><p>&nbsp;</p>`;
             const viewFragment = editor.data.processor.toView(audio);
             const modelFragment = editor.data.toModel(viewFragment);
             editor.model.insertContent(
               modelFragment,
               editor.model.document.selection
             );
+            swal.close();
+          })
+          .catch(() => {
+            swal("Info", "Terjadi kesalahan, silahkan coba kembali!", "error");
+            swal.close();
+            return;
           });
       });
       return view;
